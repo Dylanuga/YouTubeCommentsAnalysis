91dc8c6d32114be16e8bef528ff22e7c591bce5d
Dylanuga <dylanuga@gmail.com>
2024-01-10 00:21:23 +0100

    Frontend

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# YouTubeCommentsAnalysis
+# Analisis de comentarios de Youtube sobre videojuegos
+## Introducción
+El objetivo de este proyecto es analizar los comentarios de Youtube sobre videojuegos, para ello se ha utilizado la API de Youtube para obtener los comentarios de los videos de los canales de videojuegos más populares. Una vez obtenidos los comentarios se ha realizado un preprocesamiento de los mismos para eliminar las palabras que no aportan información y se ha realizado un análisis de sentimiento para obtener la polaridad de los comentarios. Finalmente se ha realizado un análisis de los resultados obtenidos.
+
+## Obtención de los comentarios
+En el script get_comments.py se ha utilizado la API de Youtube para obtener los comentarios de los videos de los canales de videojuegos más populares. Para ello se ha utilizado la librería [google-api-python-client]
--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 from flask import Flask, render_template, request
 import pandas as pd
+from config import * # Asegúrate de importar tu archivo de configuración
+from get_comments import buscar_video, get_comments
+from googleapiclient.discovery import build
+
+# Definir la clave de API
+api_key = api_key
+
+# Construir el objeto youtube usando la clave de API
+youtube = build("youtube", "v3", developerKey=api_key)
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -7,11 +17,12 @@ def index():
     if request.method == 'POST':
         game = request.form.get('game')
         if game:
-            # obtén los comentarios de YouTube aquí
-            # procesa los comentarios con pandas aquí
-            resultados = None  # reemplaza None con tus resultados procesados
-            return render_template('resultados.html', resultados=resultados)
-    return render_template('index.html')
+            video_id = buscar_video(youtube, game)
+            comments = get_comments(youtube, video_id)
+            df = pd.DataFrame(comments)
+            df.to_csv('data/comentarios_mejorados.csv', sep=',', index=False)
+            return render_template('index.html', comments=comments, rawg_key=rawg_key)
+    return render_template('index.html', rawg_key=rawg_key)
 
 if __name__ == '__main__':
     app.run(debug=True)
